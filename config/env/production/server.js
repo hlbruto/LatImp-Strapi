ffc06dfa78f1b989da0216e84c410c712b25c374
henry.perez <henry.perez@jovenclub.cu>
2022-09-27 18:14:00 -0400

    Subiendo latimpar ok

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,7 +1,14 @@
+<<<<<<< HEAD
 module.exports = ({env}) => ({
     url: env('https://latimpar.herokuapp.com/'),
     proxy: true,
     app: {
+=======
+module.exports = ({env})=({
+    url:env('https://latimpar.herokuapp.com/'),
+    proxy:true,
+    app:{
+>>>>>>> 20a08d5 (Subiendo latimpar ok)
        keys: env.array('APP_KEYS',['dcbgC7C/8ZqC7aLauTTE9g==','NWrvQgn9pYVlrV4ac4Kxmw==','HWLPVKlhdiwlK2cCX0dSvw==','3j+p0kv6/zQwnVUfi1HFiQ=='])
     }
 })
