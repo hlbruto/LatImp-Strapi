4611ac5c027db9823de970960a6aa8ebbc5eb5a7
henry.perez <henry.perez@jovenclub.cu>
2022-09-29 11:23:23 -0400

    Agregando token de admin js

--- config/admin.js
+++ config/admin.js
@@ -2,4 +2,7 @@ module.exports = ({ env }) => ({
   auth: {
     secret: env('ADMIN_JWT_SECRET', '1026f515ee846423e0f489407a37da1f'),
   },
+  apiToken: {
+    salt: env('API_TOKEN_SALT', 'mv7TLNdDWfGZJ8MKhyCBBA=='),
+  },
 });
