787f30227715ae10388cac538f593c668436b44b
henry.perez <henry.perez@jovenclub.cu>
2022-09-30 12:47:14 -0400

    Subiendo con postgres

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
-const path = require('path');
+/* const path = require('path');
 
 module.exports = ({ env }) => ({
   connection: {
@@ -7,5 +7,19 @@ module.exports = ({ env }) => ({
       filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
     },
     useNullAsDefault: true,
+  },
+});
+ */
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'postgres',
+    connection: {
+      host: env('DATABASE_HOST', '127.0.0.1'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'latimpar_db'),
+      user: env('DATABASE_USERNAME', 'postgres'),
+      password: env('DATABASE_PASSWORD', 'postgres'),
+      ssl: env.bool('DATABASE_SSL', false),
+    },
   },
 });
